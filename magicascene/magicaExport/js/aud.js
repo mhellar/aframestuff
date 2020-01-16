05d252110d2eeffb2d604fb4303615ec9bfb206b
Mark Hellar <mark.hellar@gmail.com>
2020-01-16 11:49:54 -0800

    Update aud.js

--- magicascene/magicaExport/js/aud.js
+++ magicascene/magicaExport/js/aud.js
@@ -33,7 +33,7 @@ AFRAME.registerComponent('circle', {
   },
   init: function () {
       var box = document.createElement('a-sphere');
-      box.setAttribute('shader-frog', "src:url(https://raw.githubusercontent.com/mhellar/aframestuff/gh-pages/basic4/shaders/New_Composed_Shader.json)")
+    //   box.setAttribute('shader-frog', "src:url(https://raw.githubusercontent.com/mhellar/aframestuff/gh-pages/basic4/shaders/sun.json)")
       box.setAttribute('position', 0 + ' ' + 6 + ' ' + 10);
       box.setAttribute('scale', '1800 1800 1800');
       this.el.appendChild(box);
